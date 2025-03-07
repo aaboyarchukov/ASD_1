5eb12b99d0b4cc9468b7e3f91ccbefb062062df5
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-03-08 02:01:12 +0300

    Add all methods to task with dynamic array and test to them

--- DynamicArray/dynamic_array-3.go
+++ DynamicArray/dynamic_array-3.go
@@ -17,26 +17,113 @@ func (da *DynArray[T]) Init() {
 }
 
 func (da *DynArray[T]) MakeArray(sz int) {
+	if sz < 16 {
+		sz = 16
+	}
+
+	if sz == da.capacity {
+		return
+	}
+
 	var arr = make([]T, sz)
-	//  копируем содержимое array в arr ...
+
+	// copy values
+	for i := 0; i < len(da.array); i++ {
+		indx := i
+		arr[indx] = da.array[indx]
+	}
+	// or
+	copy(arr, da.array)
 	da.capacity = sz
-	da.array = arr //
+	da.array = arr
 }
 
 func (da *DynArray[T]) Insert(itm T, index int) error {
+	if da.count == da.capacity {
+		da.MakeArray(da.capacity * 2)
+	}
+
+	if index == da.count {
+		da.Append(itm)
+		return nil
+	}
+
+	if index < da.count || index > 0 {
+		for i := da.count - 1; i >= index; i-- {
+			da.array[i+1] = da.array[i]
+		}
+		da.array[index+1] = da.array[index]
+		da.array[index] = itm
+		da.count++
+		return nil
+	}
+
 	return fmt.Errorf("bad index '%d'", index)
 }
 
 func (da *DynArray[T]) Remove(index int) error {
+	difference := float64(da.count) / float64(da.capacity)
+	var newCap float64
+	changed := false
+
+	for difference < 0.5 && da.capacity > 16 {
+		changed = true
+		newCap = float64(da.capacity) / 1.5
+		da.capacity = int(newCap)
+		difference = float64(da.count) / float64(da.capacity)
+	}
+
+	if changed {
+		da.MakeArray(int(newCap))
+	}
+
+	if index < da.count && index > 0 {
+		for i := index + 1; i < da.count; i++ {
+			indx := i
+			da.array[indx-1] = da.array[indx]
+		}
+		da.count--
+		return nil
+	}
+
 	return fmt.Errorf("bad index '%d'", index)
 }
 
 func (da *DynArray[T]) Append(itm T) {
+	if da.count == da.capacity {
+		da.MakeArray(da.capacity * 2)
+	}
 
+	da.array[da.count] = itm
+	da.count++
 }
 
 func (da *DynArray[T]) GetItem(index int) (T, error) {
 	var result T
-	// ...
+	if index >= da.count || index < 0 {
+		return result, fmt.Errorf("index out of range")
+	}
+
+	result = da.array[index]
 	return result, nil
 }
+
+func EqualArrays[T comparable](da1 *DynArray[T], da2 *DynArray[T]) bool {
+	if da1.count != da2.count || da1.capacity != da2.capacity {
+		return false
+	}
+
+	for i := 0; i < da1.count; i++ {
+		idx := i
+		if da1.array[idx] != da2.array[idx] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// func GenerateArray(count, cap int) []int {
+
+// 	return []int{}
+// }
--- DynamicArray/dynamic_array-3_test.go
+++ DynamicArray/dynamic_array-3_test.go
@@ -1,1 +1,275 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// upgrade - x2, downgrade - x1.5
+// min cap = 16
+// if after downgrade len(buffer) < 16 => new len(buffer) = 16
+// if len = cap => upgrade, if len / cap < 50% => downgrade
+
+func TestMakeArray(t *testing.T) {
+	tests := []struct {
+		name  string
+		array *DynArray[int]
+		input int
+		want  int
+	}{
+		{"Test1: ", &DynArray[int]{
+			count:    1,
+			capacity: 16,
+			array:    []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		}, 3, 16},
+		{"Test2: ", &DynArray[int]{
+			count:    16,
+			capacity: 16,
+			array:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+		}, 32, 32},
+		{"Test3: ", &DynArray[int]{
+			count:    1,
+			capacity: 16,
+			array:    []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		}, 20, 20},
+	}
+
+	for _, tempTest := range tests {
+		test := tempTest
+		test.array.MakeArray(test.input)
+		if test.array.capacity != test.want {
+			t.Errorf("failed %s: make array", test.name)
+		}
+
+	}
+}
+
+func TestAppend(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *DynArray[int]
+		want  *DynArray[int]
+		item  int
+	}{
+		{"Test1", &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    3,
+				capacity: 16,
+				array:    []int{1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 3},
+		{"Test2", &DynArray[int]{
+			count:    16,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    17,
+				capacity: 32,
+				array: []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 5},
+		{"Test3", &DynArray[int]{
+			count:    0,
+			capacity: 16,
+			array:    []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    1,
+				capacity: 16,
+				array:    []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 1},
+	}
+
+	for _, tempTest := range tests {
+		test := tempTest
+		test.input.Append(test.item)
+		if !EqualArrays(test.input, test.want) {
+			t.Errorf("failed %s: append element", test.name)
+		}
+	}
+}
+
+func TestInsert(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *DynArray[int]
+		want  *DynArray[int]
+		item  int
+		indx  int
+	}{
+		{"Test1", &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    3,
+				capacity: 16,
+				array:    []int{1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 3, 2},
+		{"Test2", &DynArray[int]{
+			count:    16,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    17,
+				capacity: 32,
+				array: []int{1, 2, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 5, 4},
+		{"Test3", &DynArray[int]{
+			count:    16,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    17,
+				capacity: 32,
+				array: []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 4, 16},
+	}
+
+	for _, tempTest := range tests {
+		test := tempTest
+		test.input.Insert(test.item, test.indx)
+		if !EqualArrays(test.input, test.want) {
+			t.Errorf("failed %s: insert element", test.name)
+		}
+	}
+}
+
+func TestRemove(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *DynArray[int]
+		want  *DynArray[int]
+		indx  int
+		err   error
+	}{
+		{"Test1", &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    1,
+				capacity: 16,
+				array:    []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 1, nil},
+		{"Test2", &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    2,
+				capacity: 16,
+				array:    []int{1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, -1, fmt.Errorf("bad index '%d'", -1)},
+		{"Test3", &DynArray[int]{
+			count:    18,
+			capacity: 64,
+			array: []int{1, 2, 0, 0, 4, 0, 0, 5, 0, 0, 0, 3, 0, 0, 1, 0,
+				7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+			&DynArray[int]{
+				count:    17,
+				capacity: 28,
+				array: []int{1, 2, 0, 0, 4, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0, 7,
+					8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			}, 7, nil},
+	}
+
+	for _, tempTest := range tests {
+		test := tempTest
+		err := test.input.Remove(test.indx)
+
+		if test.err == nil && err != nil {
+			t.Errorf("failed %s: remove element raise error", test.name)
+			continue
+		}
+
+		if !EqualArrays(test.input, test.want) {
+			t.Errorf("failed %s: remove element", test.name)
+			continue
+		}
+	}
+}
+func TestGetItem(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   *DynArray[int]
+		indx    int
+		element int
+	}{{"Test1", &DynArray[int]{
+		count:    1,
+		capacity: 16,
+		array:    []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}, 0, 1},
+		{"Test2", &DynArray[int]{
+			count:    1,
+			capacity: 16,
+			array:    []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		}, -1, 0}}
+
+	for _, tempTest := range tests {
+		test := tempTest
+		tempElement, _ := test.input.GetItem(test.indx)
+
+		if tempElement != test.element {
+			t.Errorf("failed %s: get item from array", tempTest.name)
+		}
+	}
+}
+
+func TestEqualsArrays(t *testing.T) {
+	tests := []struct {
+		name string
+		arr1 *DynArray[int]
+		arr2 *DynArray[int]
+		want bool
+	}{
+		{"Test1: ", &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2},
+		}, &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2},
+		}, true},
+		{"Test2: ", &DynArray[int]{
+			count:    4,
+			capacity: 16,
+			array:    []int{1, 2, 3, 4},
+		}, &DynArray[int]{
+			count:    2,
+			capacity: 16,
+			array:    []int{1, 2},
+		}, false},
+		{"Test3: ", &DynArray[int]{
+			count:    0,
+			capacity: 16,
+			array:    []int{},
+		}, &DynArray[int]{
+			count:    0,
+			capacity: 16,
+			array:    []int{},
+		}, true},
+	}
+
+	for _, tempTest := range tests {
+		test := tempTest
+		if EqualArrays(test.arr1, test.arr2) != test.want {
+			t.Errorf("failed %s: arrays are not equal", tempTest.name)
+		}
+	}
+}
