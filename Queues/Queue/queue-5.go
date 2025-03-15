86a12cbb65019e8b19246dfeb06ba9a9ab5ce244
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-03-15 15:23:16 +0300

    implemented queue to the end

--- Queues/Queue/queue-5.go
+++ Queues/Queue/queue-5.go
@@ -1,22 +1,89 @@
 package main
 
 import (
+	"fmt"
 	_ "os"
-	//	"fmt" включите если используете
 )
 
+type QNode[T any] struct {
+	value T
+	next  *QNode[T]
+}
+
 type Queue[T any] struct {
+	head *QNode[T]
+	tail *QNode[T]
+	size int
 }
 
 func (q *Queue[T]) Size() int {
-	return 0
+	return q.size
 }
 
+// t = O(1)
 func (q *Queue[T]) Dequeue() (T, error) {
 	var result T
-	// ...
+	if q.size == 0 {
+		return result, fmt.Errorf("queue is empty")
+	}
+
+	result = q.head.value
+	next := q.head.next
+	q.head = next
+
+	if next == nil {
+		q.tail = next
+	}
+
+	q.size--
+
 	return result, nil
 }
 
+// t = O(1)
 func (q *Queue[T]) Enqueue(itm T) {
+	node := &QNode[T]{
+		value: itm,
+		next:  nil,
+	}
+
+	if q.head == nil {
+		q.head = node
+	} else {
+		q.tail.next = node
+	}
+
+	q.tail = node
+	q.size++
+}
+
+func GetQueue[T any](values []T) *Queue[T] {
+	var result Queue[T]
+
+	for _, item := range values {
+		result.Enqueue(item)
+	}
+
+	return &result
+}
+
+func EqualQueue[T comparable](q1 *Queue[T], q2 *Queue[T]) bool {
+	if q1.size != q2.size {
+		return false
+	}
+
+	if q1 == nil && q1 == q2 {
+		return true
+	}
+
+	tempNode1, tempNode2 := q1.head, q2.head
+
+	for tempNode1 != nil {
+		if tempNode1.value != tempNode2.value {
+			return false
+		}
+		tempNode1 = tempNode1.next
+		tempNode2 = tempNode2.next
+	}
+	return true
 }
--- Queues/Queue/queue-5_test.go
+++ Queues/Queue/queue-5_test.go
@@ -1,1 +1,82 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDequeue(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *Queue[any]
+		want  *Queue[any]
+		value any
+		err   error
+	}{
+		{"Test1", GetQueue([]any{1, "row", true}), GetQueue([]any{"row", true}), 1, nil},
+		{"Test2", GetQueue([]any{}), GetQueue([]any{}), nil, fmt.Errorf("queue is empty")},
+		{"Test3", GetQueue([]any{"row"}), GetQueue([]any{}), "row", nil},
+	}
+
+	for _, test := range tests {
+		value, err_deq := test.input.Dequeue()
+
+		if value != test.value {
+			t.Errorf("failed %s: vlaue are not equal", test.name)
+		}
+		if !EqualQueue(test.input, test.want) {
+			t.Errorf("failed %s: delete last item, queues are not equal", test.name)
+		}
+
+		switch test.err {
+		case nil:
+			if err_deq != test.err {
+				t.Errorf("failed %s: different errors", test.name)
+			}
+		default:
+			if err_deq.Error() != test.err.Error() {
+				t.Errorf("failed %s: different errors", test.name)
+			}
+		}
+	}
+
+}
+func TestSize(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *Queue[any]
+		want  int
+	}{
+		{"Test1", GetQueue([]any{1, "row", true}), 3},
+		{"Test2", GetQueue([]any{}), 0},
+		{"Test3", GetQueue([]any{1}), 1},
+	}
+
+	for _, test := range tests {
+		testSize := test.input.size
+
+		if testSize != test.want {
+			t.Errorf("failed %s: get size", test.name)
+		}
+	}
+}
+func TestEnqueue(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *Queue[any]
+		value any
+		want  *Queue[any]
+	}{
+		{"Test1", GetQueue([]any{1, "row", true}), false, GetQueue([]any{1, "row", true, false})},
+		{"Test2", GetQueue([]any{1}), false, GetQueue([]any{1, false})},
+		{"Test2", GetQueue([]any{}), false, GetQueue([]any{false})},
+	}
+
+	for _, test := range tests {
+		test.input.Enqueue(test.value)
+
+		if !EqualQueue(test.input, test.want) {
+			t.Errorf("failed %s: insert item at the end", test.name)
+		}
+	}
+}
