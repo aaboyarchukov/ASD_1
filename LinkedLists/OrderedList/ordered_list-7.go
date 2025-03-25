820ae2324767f6def6df64e60f58118fe89a82fc
aaboyarchukov <101995272+aaboyarchukov@users.noreply.github.com>
2025-03-26 01:42:37 +0300

    Update ordered_list-7.go

--- LinkedLists/OrderedList/ordered_list-7.go
+++ LinkedLists/OrderedList/ordered_list-7.go
@@ -4,7 +4,7 @@ import (
 	"constraints"
 	"errors"
 	"fmt"
-	_ "os"
+	"os"
 	"strings"
 )
 
@@ -255,3 +255,5 @@ func EqualOrderedLists[T constraints.Ordered](l1 *OrderedList[T], values []T) bo
 
 	return true
 }
+
+
