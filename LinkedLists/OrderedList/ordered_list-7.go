948b57e08c36f6c5b642294334e6e851a7a82d6d
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-03-26 11:10:37 +0300

    add new test

--- LinkedLists/OrderedList/ordered_list-7.go
+++ LinkedLists/OrderedList/ordered_list-7.go
@@ -4,7 +4,7 @@ import (
 	"constraints"
 	"errors"
 	"fmt"
-	"os"
+	_ "os"
 	"strings"
 )
 
@@ -255,5 +255,3 @@ func EqualOrderedLists[T constraints.Ordered](l1 *OrderedList[T], values []T) bo
 
 	return true
 }
-
-
--- LinkedLists/OrderedList/ordered_list-7_test.go
+++ LinkedLists/OrderedList/ordered_list-7_test.go
@@ -104,7 +104,7 @@ func TestAdd(t *testing.T) {
 	type testType[T constraints.Ordered] struct {
 		name  string
 		input *OrderedList[T]
-		value T
+		value []T
 		want  []T
 	}
 
@@ -112,181 +112,237 @@ func TestAdd(t *testing.T) {
 		{
 			name:  "Test1",
 			input: GetOrderedList(true, []int64{9, 1, 5, 2, 7, 4}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{1, 2, 3, 4, 5, 7, 9},
 		},
 		{
 			name:  "Test2",
 			input: GetOrderedList(true, []int64{1}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{1, 3},
 		},
 		{
 			name:  "Test3",
 			input: GetOrderedList(true, []int64{}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{3},
 		},
 		{
 			name:  "Test4",
 			input: GetOrderedList(false, []int64{9, 1, 5, 2, 7, 4}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{9, 7, 5, 4, 3, 2, 1},
 		},
 		{
 			name:  "Test5",
 			input: GetOrderedList(false, []int64{1}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{3, 1},
 		},
 		{
 			name:  "Test6",
 			input: GetOrderedList(false, []int64{}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{3},
 		},
 		{
 			name:  "Test7",
 			input: GetOrderedList(true, []int64{9, 1, 5, 2, 7, 3, 4}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{1, 2, 3, 3, 4, 5, 7, 9},
 		},
 		{
 			name:  "Test8",
 			input: GetOrderedList(false, []int64{9, 1, 5, 2, 7, 3, 4}),
-			value: 3,
+			value: []int64{3},
 			want:  []int64{9, 7, 5, 4, 3, 3, 2, 1},
 		},
 		{
 			name:  "Test9",
 			input: GetOrderedList(true, []int64{1, 1, 1, 1}),
-			value: 1,
+			value: []int64{1},
 			want:  []int64{1, 1, 1, 1, 1},
 		},
 		{
 			name:  "Test10",
 			input: GetOrderedList(false, []int64{1, 1, 1, 1}),
-			value: 1,
+			value: []int64{1},
 			want:  []int64{1, 1, 1, 1, 1},
+		},
+		{
+			name:  "Test10",
+			input: GetOrderedList(false, []int64{1, 1, 1, 1}),
+			value: []int64{1},
+			want:  []int64{1, 1, 1, 1, 1},
+		},
+		{
+			name:  "Test11",
+			input: GetOrderedList(true, []int64{}),
+			value: []int64{100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			want:  []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100},
+		},
+		{
+			name:  "Test12",
+			input: GetOrderedList(false, []int64{}),
+			value: []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100},
+			want:  []int64{100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
 		},
 	}
 	testsFloat := []testType[float64]{
 		{
 			name:  "Test1",
 			input: GetOrderedList(true, []float64{2.1, 1.2, 5.6, 1.1}),
-			value: 3.4,
+			value: []float64{3.4},
 			want:  []float64{1.1, 1.2, 2.1, 3.4, 5.6},
 		},
 		{
 			name:  "Test2",
 			input: GetOrderedList(true, []float64{2.1}),
-			value: 3.4,
+			value: []float64{3.4},
 			want:  []float64{2.1, 3.4},
 		},
 		{
 			name:  "Test3",
 			input: GetOrderedList(true, []float64{}),
-			value: 3.4,
+			value: []float64{3.4},
 			want:  []float64{3.4},
 		},
 		{
 			name:  "Test4",
 			input: GetOrderedList(false, []float64{2.1, 1.2, 5.6, 1.1}),
-			value: 3.4,
+			value: []float64{3.4},
 			want:  []float64{5.6, 3.4, 2.1, 1.2, 1.1},
 		},
 		{
 			name:  "Test5",
 			input: GetOrderedList(false, []float64{2.1}),
-			value: 3.4,
+			value: []float64{3.4},
 			want:  []float64{3.4, 2.1},
 		},
 		{
 			name:  "Test6",
 			input: GetOrderedList(false, []float64{}),
-			value: 3.4,
+			value: []float64{3.4},
 			want:  []float64{3.4},
 		},
 		{
 			name:  "Test7",
 			input: GetOrderedList(false, []float64{1.1, 1.1, 1.1, 1.1}),
-			value: 1.1,
+			value: []float64{1.1},
 			want:  []float64{1.1, 1.1, 1.1, 1.1, 1.1},
 		},
 		{
 			name:  "Test8",
 			input: GetOrderedList(true, []float64{1.1, 1.1, 1.1, 1.1}),
-			value: 1.1,
+			value: []float64{1.1},
 			want:  []float64{1.1, 1.1, 1.1, 1.1, 1.1},
+		},
+		{
+			name:  "Test9",
+			input: GetOrderedList(true, []float64{1.1, 1.1, 1.1, 1.1, 5.4, 5.4, 3.2, 1.1, 1.1, 1.1}),
+			value: []float64{1.1},
+			want:  []float64{1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 3.2, 5.4, 5.4},
+		},
+		{
+			name:  "Test10",
+			input: GetOrderedList(false, []float64{1.1, 1.1, 1.1, 1.1, 5.4, 5.4, 3.2, 1.1, 1.1, 1.1}),
+			value: []float64{1.1},
+			want:  []float64{5.4, 5.4, 3.2, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1},
 		},
 	}
 	testsString := []testType[string]{
 		{
 			name:  "Test1",
 			input: GetOrderedList(true, []string{"  1", "3  ", " 2"}),
-			value: "12",
+			value: []string{"12"},
 			want:  []string{"1 ", "12  ", " 2", "   3"},
 		},
 		{
 			name:  "Test2",
 			input: GetOrderedList(true, []string{"1"}),
-			value: "12",
+			value: []string{"12"},
 			want:  []string{"1", "12"},
 		},
 		{
 			name:  "Test3",
 			input: GetOrderedList(true, []string{}),
-			value: "12",
+			value: []string{"12"},
 			want:  []string{"12"},
 		},
 		{
 			name:  "Test4",
 			input: GetOrderedList(false, []string{"  1", "3  ", " 2"}),
-			value: "12",
+			value: []string{"12"},
 			want:  []string{"   3", "2", "12  ", " 1"},
 		},
 		{
 			name:  "Test5",
 			input: GetOrderedList(false, []string{"1"}),
-			value: "12",
+			value: []string{"12"},
 			want:  []string{"12", "1"},
 		},
 		{
 			name:  "Test6",
 			input: GetOrderedList(false, []string{}),
-			value: "12",
+			value: []string{"12"},
 			want:  []string{"12"},
 		},
 		{
 			name:  "Test7",
 			input: GetOrderedList(false, []string{"1  ", "1", " 1"}),
-			value: "1",
+			value: []string{"1"},
 			want:  []string{"1  ", "1", " 1", "1"},
 		},
 		{
 			name:  "Test8",
 			input: GetOrderedList(true, []string{"1  ", "1", " 1"}),
-			value: "1",
+			value: []string{"1"},
 			want:  []string{"1  ", "1", " 1", "1"},
+		},
+		{
+			name:  "Test9",
+			input: GetOrderedList(true, []string{"1", "1", "1"}),
+			value: []string{"1"},
+			want:  []string{"1", "1", "1", "1"},
+		},
+		{
+			name:  "Test10",
+			input: GetOrderedList(true, []string{"1", "1", "2", "3", "3", "3", "1"}),
+			value: []string{"1"},
+			want:  []string{"1", "1", "1", "1", "2", "3", "3", "3"},
+		},
+		{
+			name:  "Test11",
+			input: GetOrderedList(true, []string{"a", " b", "v", "  a"}),
+			value: []string{"a"},
+			want:  []string{"a", "  a", "a", "b", "v"},
 		},
 	}
 
 	for _, test := range testsInt {
-		test.input.Add(test.value)
+		for _, item := range test.value {
+			test.input.Add(item)
+		}
+
 		if !EqualOrderedLists(test.input, test.want) {
 			t.Errorf("failed %s: not equals lists. Result is: ", test.name)
 			PrintList(*test.input)
 		}
+		PrintList(*test.input)
 	}
 	for _, test := range testsFloat {
-		test.input.Add(test.value)
+		for _, item := range test.value {
+			test.input.Add(item)
+		}
 		if !EqualOrderedLists(test.input, test.want) {
 			t.Errorf("failed %s: not equals lists. Result is: ", test.name)
 			PrintList(*test.input)
 		}
 	}
 	for _, test := range testsString {
-		test.input.Add(test.value)
+		for _, item := range test.value {
+			test.input.Add(item)
+		}
 		if !EqualOrderedLists(test.input, test.want) {
 			t.Errorf("failed %s: not equals lists. Result is: ", test.name)
 			PrintList(*test.input)
