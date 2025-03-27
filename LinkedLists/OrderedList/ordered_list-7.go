a47a796573054af7d4ce581317b6b4f5c63067d9
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-03-27 19:51:29 +0300

    add reflection on made mistakes and testing program

--- LinkedLists/OrderedList/ordered_list-7.go
+++ LinkedLists/OrderedList/ordered_list-7.go
@@ -4,7 +4,7 @@ import (
 	"constraints"
 	"errors"
 	"fmt"
-	"os"
+	_ "os"
 	"strings"
 )
 
@@ -264,9 +264,3 @@ func EqualOrderedLists[T constraints.Ordered](l1 *OrderedList[T], values []T) bo
 
 	return true
 }
-
-
-
-
-
-
