d2f73e945c82ffda893323255b6e72bf027d691b
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-04-13 00:50:13 +0300

    fix bugs and implement other realization of powerset

--- PowerSet/powerset-10.go
+++ PowerSet/powerset-10.go
@@ -2,61 +2,21 @@ package main
 
 import (
 	"constraints"
-	"fmt"
-	"os"
-	"strconv"
+	_ "os"
+	"slices"
+	_ "strconv"
 )
 
 type PowerSet[T constraints.Ordered] struct {
-	cap       int
-	slots     []T
-	fillSlots []bool
-	step      int
+	cap   int
+	slots []T
 }
 
 func Init[T constraints.Ordered]() PowerSet[T] {
 	return PowerSet[T]{
-		cap:       0,
-		slots:     make([]T, 20000),
-		fillSlots: make([]bool, 20000),
-		step:      3,
+		cap:   0,
+		slots: make([]T, 0, 20000),
 	}
-}
-
-func (ps *PowerSet[T]) SeekSlot(value T) int {
-
-	hash := ps.HashFunc(value)
-
-	if !ps.fillSlots[hash] {
-		return hash
-	}
-
-	if ps.cap < len(ps.slots) {
-
-		resultIndx, indx := hash, hash
-		for ps.fillSlots[resultIndx] {
-			indx += ps.step
-			resultIndx = indx % len(ps.slots)
-		}
-
-		return resultIndx
-	}
-
-	return -1
-}
-
-func (ps *PowerSet[T]) HashFunc(value T) int {
-	valueForHash := fmt.Sprintf("%v", value)
-
-	var indx int
-	var sum byte
-	for i, item := range valueForHash {
-		sum += byte(item) * byte(i)
-	}
-
-	indx = int(sum) % len(ps.slots)
-
-	return indx
 }
 
 func (ps *PowerSet[T]) Size() int {
@@ -65,9 +25,7 @@ func (ps *PowerSet[T]) Size() int {
 
 func (ps *PowerSet[T]) Put(value T) {
 	if ps.cap < len(ps.slots) && !ps.Get(value) {
-		hash := ps.SeekSlot(value)
-		ps.slots[hash] = value
-		ps.fillSlots[hash] = true
+		ps.slots = append(ps.slots, value)
 		ps.cap++
 	}
 
@@ -75,14 +33,14 @@ func (ps *PowerSet[T]) Put(value T) {
 }
 
 func (ps *PowerSet[T]) Get(value T) bool {
-	hash := ps.SeekSlot(value)
-	return ps.fillSlots[hash]
+	return slices.Contains(ps.slots, value)
 }
 
 func (ps *PowerSet[T]) Remove(value T) bool {
 	if ps.Get(value) {
-		hash := ps.SeekSlot(value)
-		ps.fillSlots[hash] = false
+		indx := slices.Index(ps.slots, value)
+		ps.slots = slices.Delete(ps.slots, indx, indx+1)
+		ps.cap--
 		return true
 	}
 
@@ -91,27 +49,57 @@ func (ps *PowerSet[T]) Remove(value T) bool {
 
 func (ps *PowerSet[T]) Intersection(set2 PowerSet[T]) PowerSet[T] {
 	var result PowerSet[T]
+	for _, val := range ps.slots {
+		if set2.Get(val) {
+			result.Put(val)
+		}
+	}
 	return result
 }
 
-func (*PowerSet[T]) Union(set2 PowerSet[T]) PowerSet[T] {
+func (ps *PowerSet[T]) Union(set2 PowerSet[T]) PowerSet[T] {
 	var result PowerSet[T]
+	for _, val := range ps.slots {
+		result.Put(val)
+	}
+
+	for _, val := range set2.slots {
+		result.Put(val)
+	}
+
 	return result
 }
 
-func (*PowerSet[T]) Difference(set2 PowerSet[T]) PowerSet[T] {
+func (ps *PowerSet[T]) Difference(set2 PowerSet[T]) PowerSet[T] {
 	var result PowerSet[T]
+	for _, val := range ps.slots {
+		if !set2.Get(val) {
+			result.Put(val)
+		}
+	}
 	return result
 }
 
-func (*PowerSet[T]) IsSubset(set2 PowerSet[T]) bool {
-	return false
+func (ps *PowerSet[T]) IsSubset(set2 PowerSet[T]) bool {
+	for _, val := range set2.slots {
+		if !ps.Get(val) {
+			return false
+		}
+	}
+
+	return true
 }
 
+func (ps *PowerSet[T]) Equals(set2 PowerSet[T]) bool {
+	if ps.cap != set2.cap {
+		return false
+	}
 
+	for _, val := range ps.slots {
+		if !set2.Get(val) {
+			return false
+		}
+	}
 
-
-
-func (*PowerSet[T]) Equals(set2 PowerSet[T]) bool {
-	return false
+	return true
 }
