2831261807998217bc2f58e3ac52ff7e1f862b52
aaboyarchukov <aaboyarchukov@edu.hse.ru>
2025-04-27 16:40:23 +0300

    add func to get min element from dequeue for O(1)

--- Queues/Dequeue/dequeue-6-2.go
+++ Queues/Dequeue/dequeue-6-2.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+
+	"constraints"
+)
+
 // task 2
 // Same time complition
 // Use doubly linked list
@@ -8,6 +15,171 @@ package main
 // tests are added
 
 // task 4
+// Is pallindrom
+// move from tail to head and comparison values
+
+// t = O(n), where n = len(row), mem = O(1)
+func RowToDequeue(row string) Deque[string] {
+	var result Deque[string]
+
+	for _, item := range row {
+		result.AddTail(string(item))
+	}
+
+	return result
+}
+
+// t = O(n), where n = len(row), mem = O(n), where n = len(row)
+func IsPallindrom(row string) bool {
+	d := RowToDequeue(row)
+
+	for d.size > 1 {
+		head, err_head := d.RemoveFront()
+		tail, err_tail := d.RemoveTail()
+		if err_head != nil || err_tail != nil {
+			return false
+		}
+
+		if strings.Compare(head, tail) != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
 // task 5
+// Min element for O(1)
+// Add min element for every node
+type MinDequeNode[T constraints.Ordered] struct {
+	value T
+	next  *MinDequeNode[T]
+	min   T
+}
+
+type MinDeque[T constraints.Ordered] struct {
+	head *MinDequeNode[T]
+	tail *MinDequeNode[T]
+	size int
+}
+
+func (d *MinDeque[T]) Size() int {
+	return d.size
+}
+
+func (d *MinDeque[T]) AddFront(itm T) {
+	node := &MinDequeNode[T]{
+		value: itm,
+		next:  nil,
+	}
+
+	min, err_get_min := d.GetMin()
+	if err_get_min != nil || itm < min {
+		min = itm
+	}
+
+	node.min = min
+
+	if d.head == nil {
+		d.tail = node
+	} else {
+		node.next = d.head
+	}
+
+	d.head = node
+	d.size++
+
+}
+
+func (d *MinDeque[T]) AddTail(itm T) {
+	node := &MinDequeNode[T]{
+		value: itm,
+		next:  nil,
+	}
+
+	min, err_get_min := d.GetMin()
+	if err_get_min != nil || itm < min {
+		min = itm
+	}
+
+	node.min = min
+
+	if d.tail == nil {
+		d.head = node
+	} else {
+		d.tail.next = node
+	}
+
+	d.tail = node
+	d.size++
+
+}
+
+func (d *MinDeque[T]) RemoveFront() (T, error) {
+	var resultNode *MinDequeNode[T] = &MinDequeNode[T]{}
+
+	if d.size == 0 {
+		return resultNode.value, fmt.Errorf("dequeue is empty")
+	}
+
+	resultNode = d.head
+	if d.size == 1 {
+		d.head, d.tail = nil, nil
+	} else {
+		currentNode := d.head
+		d.head = d.head.next
+		currentNode.next = nil
+	}
+
+	d.size--
+
+	return resultNode.value, nil
+}
+
+func (d *MinDeque[T]) RemoveTail() (T, error) {
+	var result T
+	if d.size == 0 {
+		return result, fmt.Errorf("dequeue is empty")
+	}
+
+	result = d.tail.value
+	if d.size == 1 {
+		d.head = nil
+		d.tail = nil
+	} else {
+		tempNode := d.head
+		for tempNode.next != d.tail {
+			tempNode = tempNode.next
+		}
+		tempNode.next = nil
+		d.tail = tempNode
+	}
+
+	d.size--
+	return result, nil
+}
+
+func (d *MinDeque[T]) GetMin() (T, error) {
+	var result T
+	if d.Size() == 0 {
+		return result, fmt.Errorf("dequeue is empty")
+	}
+
+	if d.head.min < d.tail.min {
+		return d.head.min, nil
+	}
+
+	return d.tail.min, nil
+}
+
+func GetMinDequeue[T constraints.Ordered](values []T) MinDeque[T] {
+	var result MinDeque[T]
+
+	for _, item := range values {
+		result.AddTail(item)
+	}
+
+	return result
+}
 
 // task 6
--- Queues/Dequeue/dequeue-6-2_test.go
+++ Queues/Dequeue/dequeue-6-2_test.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestPallindrom(t *testing.T) {
+	tests := []struct {
+		name string
+		row  string
+		want bool
+	}{
+		{"Test1", "abba", true},
+		{"Test2", "abcba", true},
+		{"Test3", "abcxba", false},
+		{"Test4", "a", true},
+		{"Test5", "ab", false},
+		{"Test6", "aa", true},
+		{"Test7", "", true},
+	}
+
+	for _, test := range tests {
+		if IsPallindrom(test.row) != test.want {
+			t.Errorf("%s: failed check pallindrom", test.name)
+		}
+	}
+}
+
+func TestGetMin(t *testing.T) {
+	tests := []struct {
+		name    string
+		deq     MinDeque[int]
+		wantMin int
+		err     bool
+	}{
+		{"Test1", GetMinDequeue([]int{3, 1, 2, 4, 6, 1}), 1, false},
+		{"Test2", GetMinDequeue([]int{}), 1, true},
+	}
+
+	for _, test := range tests {
+		min, err_get_min := test.deq.GetMin()
+
+		if !test.err && err_get_min != nil {
+			t.Errorf("%s: failed get min, raise error", test.name)
+			continue
+		}
+
+		if test.err && err_get_min == nil {
+			t.Errorf("%s: failed get min, raise error", test.name)
+			continue
+		}
+
+		if min != test.wantMin && err_get_min == nil {
+			t.Errorf("%s: failed get min", test.name)
+		}
+	}
+}
